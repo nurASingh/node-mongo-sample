818636d781b0a8d79ce1cdf88870a5baa967bfa0
unknown <arun.enjoys@gmail.com>
2016-10-11 01:56:47 +0530

    mongo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -20,6 +21,20 @@ app.get('/number', function(request, response) {
 app.get('/name', function(request, response) {
   response.send('Arun');
 });
+
+var dbname = 'mongodb://arun:123456@ds053156.mlab.com:53156/shubhlabh';
+app.get('/getdb', function(request, response) {
+  MongoClient.connect(dbname, function(err, db) {
+  if(!err) {
+    response.send("We are connected");
+  }
+});
+});
+
+
+
+// Connect to the db
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
